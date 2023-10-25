ff146ebc75a1f3e4eb640ee6693b4eab04558732
castrogarciajs <johancs.mm@gmail.com>
2023-10-24 22:13:51 -0500

    fix: remove import file svg

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,5 @@
 import { component$, useSignal } from '@builder.io/qwik'
-
 import qwikLogo from './assets/qwik.svg'
-import viteLogo from '/vite.svg'
 
 export const App = component$(() => {
   const count = useSignal(0)
@@ -9,18 +7,15 @@ export const App = component$(() => {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://qwik.builder.io" target="_blank">
-          <img src={qwikLogo} class="logo qwik" alt="Qwik logo" />
+        <a href='https://qwik.builder.io' target='_blank'>
+          <img src={qwikLogo} class='logo qwik' alt='Qwik logo' />
         </a>
       </div>
       <h1>Vite + Qwik</h1>
-      <div class="card">
+      <div class='card'>
         <button onClick$={() => count.value++}>count is {count.value}</button>
       </div>
-      <p class="read-the-docs">
+      <p class='read-the-docs'>
         Click on the Vite and Qwik logos to learn more
       </p>
     </>
