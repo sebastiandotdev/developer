e13b156ec11f2373595c04cd39cc26c9af1bae3a
castrogarciajs <johancs.mm@gmail.com>
2023-10-26 11:04:48 -0500

    style: remove comment unneccesary

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -1,15 +1,3 @@
-/**
- * WHAT IS THIS FILE?
- *
- * SSR entry point, in all cases the application is rendered outside the browser, this
- * entry point will be the common one.
- *
- * - Server (express, cloudflare...)
- * - npm run start
- * - npm run preview
- * - npm run build
- *
- */
 import {
   renderToStream,
   type RenderToStreamOptions,
@@ -21,7 +9,6 @@ export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
     ...opts,
-    // Use container attributes to set attributes on the html tag.
     containerAttributes: {
       lang: 'en-us',
       ...opts.containerAttributes,
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
 import { component$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
+import 'lib/supabase'
 
 export default component$(() => {
   return <h1 class='text-xl'>Hello qwik</h1>
 })
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Welcome to Social Developer',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'The social developer red social for developer.',
     },
   ],
 }
