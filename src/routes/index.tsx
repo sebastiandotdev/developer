8a789b0027650e7d3016e6b626d85bcf13ea331d
castrogarciajs <johancs.mm@gmail.com>
2023-10-26 20:23:48 -0500

    feat: add ui container

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,56 @@
 import { component$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
-import 'lib/supabase'
+import rocketSvg from '../media/svg/rocket.svg'
+import bellSvg from '../media/svg/bell.svg'
+import githubPng from '../media/github.png'
 
 export default component$(() => {
-  return <h1 class='text-xl'>Hello qwik</h1>
+  return (
+    <>
+      <header class='flex items-center justify-end h-[--size] px-4 sm:px-6 lg:px-8 max-w-7xl'>
+        <a href='#' class='pt-6'>
+          <img
+            src={githubPng}
+            alt='Github profile castrogarciajs'
+            width={28}
+            height={28}
+          />
+        </a>
+      </header>
+      <section class='py-24 sm:py-32 md:py-40 relative'>
+        <article class='mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl gap-16 sm:gap-y-24 flex flex-col'>
+          <div class='relative z-[1] text-center'>
+            <h1 class='text-5xl font-bold tracking-tight text-black sm:text-7xl font-poppins'>
+              The Intuitive
+              <br />
+              <span class='text-aqua block lg:inline-block'>
+                Social Developer
+              </span>
+            </h1>
+            <p class='mt-6 text-lg tracking-tight'>
+              The red social Open Sources Built with love and nostalgia 🐱
+            </p>
+            <div class='mt-10 flex flex-wrap gap-x-6 gap-y-3 justify-center'>
+              <a
+                href='#'
+                class='rounded-md text-md gap-x-2.5 px-4 py-2.5 shadow-sm text-white bg-aqua inline-flex items-center font-poppins'
+              >
+                <img src={rocketSvg} alt='rocket' width={28} height={28} />
+                Sing In
+              </a>
+              <a
+                href='#'
+                class='text-base gap-x-2.5 px-4 py-2.5 shadow-md inline-flex items-center font-poppins rounded-md'
+              >
+                <img src={bellSvg} alt='bell' width={28} height={28} />
+                Sing Up
+              </a>
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
+  )
 })
 
 export const head: DocumentHead = {
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,8 @@
 import { component$, Slot } from '@builder.io/qwik'
 import { routeLoader$ } from '@builder.io/qwik-city'
 import type { RequestHandler } from '@builder.io/qwik-city'
+import qwikSvg from '../media/svg/qwik.svg'
+import githubPng from '../media/github.png'
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   cacheControl({
@@ -18,9 +20,35 @@ export const useServerTimeLoader = routeLoader$(() => {
 export default component$(() => {
   return (
     <>
-      <main>
+      <main class='min-h-[calc(100vh-var(--size))]'>
         <Slot />
       </main>
+      <footer class='w-11/12 max-w-5xl mx-auto mt-24 sm:!mt-28 mb-8 flex items-center justify-between'>
+        <span class='flex items-center gap-4'>
+          <a
+            href='https://qwik.builder.io'
+            class='flex gap-2 items-center font-poppins bg-white rounded-md p-1.5'
+          >
+            <img src={qwikSvg} alt='Nextjs' width={20} height={20} />
+            Made with Qwik
+          </a>
+          <a
+            class='hover:underline flex gap-2 items-center font-poppins'
+            href='https://github.com/network'
+          >
+            Source
+            <img src={githubPng} alt='Github' width={20} height={20} />
+          </a>
+        </span>
+
+        <a
+          href='https://github.com/castrogarciajs'
+          target='_blank'
+          class='text-gray-600 hover:underline font-poppins'
+        >
+          castrogarciajs
+        </a>
+      </footer>
     </>
   )
 })
