207737eb588f1ddbb7f5faa0a1c197e92813ea37
castrogarciajs <johancs.mm@gmail.com>
2023-10-26 20:22:27 -0500

    chore: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,18 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      ice: '#E6EEFA',
+      some: '#6C7A9C',
+      sad: '#E9E9EB',
+      aqua: '#5790DF',
+      indigo: '#093D89',
+    },
   },
   plugins: [],
 }
