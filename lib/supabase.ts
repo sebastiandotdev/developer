f35976c0dd75ca6180f3ad42237a209170f34119
castrogarciajs <johancs.mm@gmail.com>
2023-10-26 20:22:52 -0500

    feat: simple config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,3 +1,10 @@
-// # config supabase
+import { createClient } from '@supabase/supabase-js'
 
-console.log('supabase')
+const URL = import.meta.env.VITE_SUPABASE_URL
+const KEY = import.meta.env.VITE_SUPABASE_KEY
+
+if (!URL && !KEY) throw new Error('Missing Supabase URL or Key')
+
+const supabase = createClient(URL, KEY)
+
+export default supabase
